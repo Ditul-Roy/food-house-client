6734a042b44cef43809d7ef548a0370998f58a62
Ditul-Roy <ditulr@gmail.com>
2023-05-03 12:36:56 +0600

    responsive navbar added

--- src/pages/Home/Facility/Facility.jsx
+++ src/pages/Home/Facility/Facility.jsx
@@ -6,9 +6,9 @@ const Facility = () => {
         <div className='my-10 mx-10 text-center'>
            <h1 className='text-3xl text bold text-amber-800 text-center'>Our facility</h1> 
            <p>we are provide facilities of customer</p>
-           <div className='flex justify-around my-10'>
+           <div className='lg:flex justify-around my-10'>
             <p className=''><FaCoffee className='text-8xl ms-16'></FaCoffee><span className='text-xl text-center text-xl'>We are serving clean coffee</span></p>
-            <p><FaHotel className='text-8xl '></FaHotel><span className='text-center'>Our own hotel</span></p>
+            <p><FaHotel className='text-8xl '></FaHotel><span className='text-center sm:ms-10'>Our own hotel</span></p>
             <p><FaHospitalAlt className='text-8xl ms-10'></FaHospitalAlt><span className='text-center text-xl'>We have own hospital</span></p>
            </div>
         </div>
--- src/shared/NavigationBar/NavigationBar.jsx
+++ src/shared/NavigationBar/NavigationBar.jsx
@@ -1,11 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { UserContext } from '../../provider/AuthContextProvider';
+import { FaList, FaRegWindowClose } from 'react-icons/fa';
 
 const NavigationBar = () => {
-    const {user, logOut } = useContext(UserContext);
+    const [open, setOpen] = useState(false);
+
+    const { user, logOut } = useContext(UserContext);
+
     const navigate = useNavigate()
-    const handleSignOut = () =>{
+    const handleSignOut = () => {
         logOut();
         <Navigate to="/chef"></Navigate>
     }
@@ -13,21 +17,29 @@ const NavigationBar = () => {
         <div>
             <div className="navbar w-full bg-amber-50">
                 <div className="navbar-start">
-                    <Link to='/chef' className="btn btn-ghost normal-case text-2xl">Food House</Link>
+                    <Link to='/chef' className="btn btn-ghost normal-case text-2xl ">Food House</Link>
                 </div>
-                <div className="navbar-center text-slate-600 lg:flex">
-                    <Link to="/chef" className='text-wite text-xl me-8'>Home</Link>
-                    <Link className='text-wite text-xl me-8'>Blog</Link>
-                </div>
-                <div className="navbar-end">
-                    {
-                        user ? <>
-                            <img className='h-12 w-12 rounded' title={user.displayName} src={user.photoURL} alt="" />
-                            <Link onClick={handleSignOut} navigate className='btn btn-warning'>SignOut</Link>
-                        </>
-                         : <Link to="/login" className='btn btn-warning'>Log in</Link>
-                    }
-                </div>
+                <div onClick={() => setOpen(!open)} className=' lg:hidden ms-20'>
+                    <span>
+                        {open === true ? <FaRegWindowClose className="h-6 w-6 text-blue-500" />
+                            : <FaList className="h-6 w-6 text-blue-500" />}
+                    </span>
+                 </div>   
+                    <ul className={`px-1 lg:flex justify-between absolute lg:static duration-500 ${open ? 'top-30 ' : '-top-36'}`}>
+                        <div className="navbar-center text-slate-600 lg:flex">
+                            <Link to="/chef" className='text-wite text-xl lg:me-8'>Home</Link>
+                            <Link className='text-wite text-xl lg:me-8'>Blog</Link>
+                        </div>
+                        <div className="navbar-end">
+                            {
+                                user ? <>
+                                    <img className='h-12 w-12 rounded' title={user.displayName} src={user.photoURL} alt="" />
+                                    <Link onClick={handleSignOut} navigate className='btn btn-warning'>SignOut</Link>
+                                </>
+                                    : <Link to="/login" className='btn btn-warning'>Log in</Link>
+                            }
+                        </div>
+                    </ul>
             </div>
         </div>
     );
